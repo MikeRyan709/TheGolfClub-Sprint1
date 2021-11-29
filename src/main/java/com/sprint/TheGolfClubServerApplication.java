7b4b0426bd4de4189373042f9d902a1146adaffc
Mike Ryan <mike.ryan@keyin.com>
2021-11-29 18:16:00 -0330

    changed application pathway

--- src/main/java/com/sprint/TheGolfClubServerApplication.java
+++ src/main/java/com/sprint/TheGolfClubServerApplication.java
@@ -1,4 +1,4 @@
-package com.sprint.TheGolfClubServer;
+package com.sprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
